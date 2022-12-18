5e564e0e9f24f6c5d2b843d9b603445b069757ae
ravikant333 <ravikant@chingari.io>
2022-12-18 20:07:15 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [pubkey,setpubkey]=useState('')
+  useEffect(() => {
+  const pubkey=window?.xnft?.solana?.publicKey?.toString();
+  setpubkey(pubkey)
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {pubkey}
         </a>
       </header>
     </div>
